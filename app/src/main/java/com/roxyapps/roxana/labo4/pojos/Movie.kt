c7d49165f5bec5e563af30af426dbd3e79bda4d0
RoxanaCortez <00502211@uca.edu.sv>
2019-04-05 16:25:16 -0600

    listo

---
+++ app/src/main/java/com/roxyapps/roxana/labo4/pojos/Movie.kt
@@ -0,0 +1,16 @@
+package com.roxyapps.roxana.labo4.pojos
+
+data class Movie (
+    val Title:String = "N/A",
+    val Year:String = "N/A",
+    val Released:String = "N/A",
+    val Runtime:String = "N/A",
+    val Genre:String = "N/A",
+    val Director:String = "N/A",
+    val Actors:String = "N/A",
+    val Plot:String = "N/A",
+    val Language:String = "N/A",
+    val imdbRating:String = "N/A",
+    val Poster:String = "N/A"
+
+)
